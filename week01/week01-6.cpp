d67913493a93cf749832022c90001c76c3faaaf6
Jnt10 <janetchen1011@gmail.com>
2024-09-12 16:29:49 +0800

    week01

---
+++ week01/week01-6.cpp
@@ -0,0 +1,14 @@
+/// week01-6.cpp 要複習迴圈、倒過來的迴圈、陣列
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[10] = {0,1,2,3,4,5,6,7,8,9};
+    for(int i=0; i<10; i++){
+        cout << a[i];
+    }
+    cout << "\n";
+    for(int i=10-1; i>=0; i--){
+        cout << a[i];
+    }
+}
