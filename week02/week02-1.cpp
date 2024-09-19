4fb3d27cd1795949c40f95a5463e493f7301f030
Jnt10 <janetchen1011@gmail.com>
2024-09-19 15:08:07 +0800

    week02

---
+++ week02/week02-1.cpp
@@ -0,0 +1,15 @@
+// week02-1.cpp
+//SOIT107_Advance_006
+// not OK!!!!
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	
+	cout << s;
+	cout << "\n";
+}
